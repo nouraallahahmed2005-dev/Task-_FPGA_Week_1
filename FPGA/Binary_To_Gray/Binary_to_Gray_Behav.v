5b31deb06b36ae5b243ed3587e019d8367a945ca
Noura_Ahmed <nouraallah.ahmed2005@gmail.com>
2026-07-31 12:39:01 -0700

    Add files via upload

---
+++ FPGA/Binary_To_Gray/Binary_to_Gray_Behav.v
@@ -0,0 +1,15 @@
+module Binary_to_Gray_Behav
+(
+    input  wire [3:0] bin,
+    output reg  [3:0] gray
+);
+
+always @(*)
+begin
+    gray[3] = bin[3];
+    gray[2] = bin[3] ^ bin[2];
+    gray[1] = bin[2] ^ bin[1];
+    gray[0] = bin[1] ^ bin[0];
+end
+
+endmodule
---
+++ FPGA/Binary_To_Gray/Binary_to_Gray_Dataflow.v
@@ -0,0 +1,12 @@
+module Binary_to_Gray_Dataflow
+(
+    input  wire [3:0] bin,
+    output wire [3:0] gray
+);
+
+assign gray[3] = bin[3];
+assign gray[2] = bin[3] ^ bin[2];
+assign gray[1] = bin[2] ^ bin[1];
+assign gray[0] = bin[1] ^ bin[0];
+
+endmodule
